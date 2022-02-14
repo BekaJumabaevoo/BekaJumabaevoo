d24d50562167f41ef4532adb8dd3778461dd2010
beka <zumabaevbeka0@gmail.com>
2022-02-15 00:35:46 +0600

    it-academy

---
+++ script.js
@@ -0,0 +1,42 @@
+// // 1 задача
+// const numOne = [0, 2, 3, 4, 5, 6, 7, 8, 9];
+// const numTwo = numOne.map((num) => {
+//   return num + 20;
+// });
+// console.log(numTwo);
+
+// // 2 задача
+
+// // 3 задача
+
+// let a = 1;
+// let result;
+// switch (a) {
+//   case 1:
+//     console.log((result = "Зима"));
+//     break;
+//   case 2:
+//     console.log((result = "Весна"));
+//     break;
+//   case 3:
+//     console.log((result = "Лето"));
+//     break;
+//   case 4:
+//     console.log((result = "Осень"));
+//     break;
+// }
+
+// //4
+// const obj = { green: "зеленый", red: "красный", blue: "голубой" };
+// for (variable in obj) {
+//   console.log(variable + " = " + obj[variable]);
+// }
+
+// // 5
+
+// let user = prompt("Ваше имя", " ");
+// if (user == 'Nasiba') {
+//   alert("Приветствую " + user);
+// } else {
+//   alert("Я Вас не знаю");
+// }
